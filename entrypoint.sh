9bfbabc5d74d609194e80288a3450992e32b476a
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-06-27 10:01:35 +0300

    nginx : wait 1m for redmine to start xibo : script cleanup docker-compose : add ssl cert generation + cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -37,29 +37,10 @@ then
     dl_xibo
   fi
   
-  # Drop the CMS cache (if it exists)
-#   if [ -d /var/www/xibo/cache ]
-#   then
-#     rm -r /var/www/xibo/cache
-#   fi 
-#   
-#   curl -SL https://github.com/xibosignage/xibo-cms/archive/${XIBO_VERSION}.tar.gz \
-#   | tar -xzC /var/www/xibo --exclude=settings.php --strip=1
-# 
-#   chown www-data.www-data -R /var/www/xibo
-# 
-#   mkdir /var/www/xibo/cache
-#   mkdir -p /var/www/xibo/library/temp
-#   chown www-data.www-data -R /var/www/xibo/cache /var/www/xibo/library
-  
   if [ ! -e "/var/www/xibo/xibo_release.txt" ]
   then
-    # This is a fresh install so bootstrap the whole
-    # system
+    # This is a fresh install so bootstrap the whole system
     echo "New install"
-#     mkdir -p /var/www/xibo/cache
-#     mkdir -p /var/www/xibo/library/temp
-#     chown www-data.www-data -R /var/www/xibo/cache /var/www/xibo/library
   
     dl_xibo
     
